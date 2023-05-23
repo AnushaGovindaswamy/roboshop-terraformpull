a5c4c32f491897138f009a0502ae43bd07ea5774
anusha <anushag2k16@gmail.com>
2023-05-23 19:15:37 +0530

    committing to elk

--- module/locals.tf
+++ module/locals.tf
@@ -11,4 +11,13 @@ locals {
     "ansible-pull -i localhost, -U https://github.com/AnushaGovindaswamy/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
 
   ]
+  db_tags = {
+      Name = "${var.component_name}-${var.env}"
+    }
+    app_tags = {
+      Name      = "${var.component_name}-${var.env}"
+      Monitor   = "true"
+      component = var.component_name
+      env       = var.env
+    }
 }
